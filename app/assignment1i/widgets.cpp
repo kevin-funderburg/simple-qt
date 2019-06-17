199bbca5ced3893db85c97fdb4ba537cdb0c2f92
Kevin Funderburg <ksfunderburg@icloud.com>
2019-06-16 23:37:25 -0500

    ready to turn in

--- app/assignment1i/widgets.cpp
+++ app/assignment1i/widgets.cpp
@@ -6,49 +6,145 @@
 #include <QDial>
 #include <QLCDNumber>
 #include <QPushButton>
+#include <QRadioButton>
 #include <QWidget>
+#include <QString>
+#include <QLabel>
 
 #include "window.h"
+#include "lcdrange.h"
+
+LCDRange::LCDRange(QWidget *parent)
+        : QWidget(parent)
+{
+    QLCDNumber *lcd = new QLCDNumber(2);
+    lcd->setSegmentStyle(QLCDNumber::Filled);
+
+    slider = new QSlider(Qt::Horizontal);
+    // Connect the signal change of the slider to the LCD output
+    connect(slider, SIGNAL(valueChanged(int)),
+            lcd, SLOT(display(int)));
+    // Connect the signal change of the slider to object itself
+    connect(slider, SIGNAL(valueChanged(int)),
+            this, SIGNAL(valueChanged(int)));
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(lcd);
+    layout->addWidget(slider);
+    setLayout(layout);
+}
+
+// Public function to get the value of a LCD/slider combo
+int LCDRange::value() const
+{
+    return slider->value();
+}
+// Public function to adjust the value of a LCD/slider combo
+void LCDRange::setValue(int value)
+{
+    slider->setValue(value);
+}
+
+void LCDRange::setRange(int minValue, int maxValue)
+{
+    if (minValue < 0 || maxValue > 99 || minValue > maxValue) {
+        qWarning("LCDRange::setRange(%d, %d)\n"
+                 "\tRange must be 0..99\n"
+                 "\tand minValue must not be greater than maxValue",
+                 minValue, maxValue);
+        return;
+    }
+    slider->setRange(minValue, maxValue);
+}
+
+class DialRange : public QWidget
+{
+    public:
+        DialRange(QWidget *parent = 0);
+};
+
+DialRange::DialRange(QWidget *parent)
+    : QWidget(parent)
+{
+    QLCDNumber *lcd = new QLCDNumber(2);
+    lcd->setSegmentStyle(QLCDNumber::Filled);
+
+    QDial *dial = new QDial;
+    dial->setNotchesVisible(true);
+    dial->setMinimum(0);
+    dial->setMaximum(99);
+    dial->setValue(49);
+    // Connect the dial's signal change to the LCD to output
+    // the dials value
+    connect(dial, SIGNAL(valueChanged(int)),
+            lcd, SLOT(display(int)));
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(lcd);
+    layout->addWidget(dial);
+    setLayout(layout);
+}
+
 
 MyWidget::MyWidget(QWidget *parent)
 : QWidget(parent)
 {
     // Quit
     QPushButton *quit = new QPushButton(tr("Quit"));
-    quit->setFont(QFont("Helvetica", 12));
-    // Volume
-    QSlider *volslider = new QSlider(Qt::Horizontal);
-    volslider->setRange(0, 99);
-    volslider->setValue(0);
-    QLCDNumber *vollcd = new QLCDNumber(2);
-    vollcd->setSegmentStyle(QLCDNumber::Filled);
-    // FM
-    QDial *fmdial = new QDial;
-    fmdial->setNotchesVisible(true);
-    fmdial->setMinimum(0);
-    fmdial->setMaximum(100);
-    QLCDNumber *fmlcd = new QLCDNumber(3);
-    // AM
-    QSlider *amslider = new QSlider(Qt::Vertical);
-    amslider->setRange(0, 10);
-    amslider->setValue(5);
-    QLCDNumber *amlcd = new QLCDNumber(3);
-
+    quit->setFont(QFont("Helvetica", 18, QFont::Bold));
+    // Connect the signal of the button click to the quit action
     connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
-    connect(volslider, SIGNAL(valueChanged(int)),
-    vollcd, SLOT(display(int)));
-    connect(fmdial, SIGNAL(valueChanged(int)),
-    fmlcd, SLOT(display(int)));
-    connect(amslider, SIGNAL(valueChanged(int)),
-    amlcd, SLOT(display(int)));
+
+    LCDRange *frequency = new LCDRange;
+    frequency->setRange(0,99);
+    frequency->setValue(45);
+    LCDRange *volume = new LCDRange;
+    volume->setRange(0,99);
+    volume->setValue(75);
+
+    QRadioButton *fm = new QRadioButton("FM");
+    QRadioButton *am = new QRadioButton("AM");
+    QGridLayout *grid = new QGridLayout;
+    // Iteratively add the widgets into desired locations
+    for (int row = 0; row < 3; ++row) {
+        for (int column = 0; column < 3; ++column) {
+            switch (row) {
+                case 0: {
+                    switch (column) {
+                        case 0: grid->addWidget(fm, row, column); break;
+                        case 2: grid->addWidget(am, row, column); break;
+                    }
+                    break;
+                }
+                case 1: {
+                    DialRange *dialRange = new DialRange;
+                    grid->addWidget(dialRange, row, column);
+                    break;
+                }
+                case 2: {
+                    QString txt;
+                    switch (column) {
+                        case 0: txt = "Bass"; break;
+                        case 1: txt = "Mid"; break;
+                        case 2: txt = "Treble"; break;
+                    }
+                    QLabel *lbl = new QLabel(txt);
+                    grid->addWidget(lbl, row, column);
+                }
+                default: break;
+            }
+        }
+    }
+    // Add labels to the sliders so you can tell which slider does what
+    QLabel *freqLbl = new QLabel("Frequency");
+    QLabel *volLbl = new QLabel("Volume");
 
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(quit);
-    layout->addWidget(vollcd);
-    layout->addWidget(volslider);
-    layout->addWidget(fmdial);
-    layout->addWidget(fmlcd);
-    layout->addWidget(amslider);
-    layout->addWidget(amlcd);
+    layout->addWidget(volLbl);
+    layout->addWidget(volume);
+    layout->addWidget(freqLbl);
+    layout->addWidget(frequency);
+    layout->addLayout(grid);
     setLayout(layout);
 }
--- app/assignment1i/window.h
+++ app/assignment1i/window.h
@@ -5,25 +5,22 @@
 
 class QGroupBox;
 
-class Window : public QWidget
-{
-    Q_OBJECT
-
-    public:
-        Window(QWidget *parent = 0);
-
-    private:
-        QGroupBox *createFirstExclusiveGroup();
-        QGroupBox *createSecondExclusiveGroup();
-        QGroupBox *createNonExclusiveGroup();
-        QGroupBox *createPushButtonGroup();
-};
+//class Window : public QWidget
+//{
+//    Q_OBJECT
+//
+//public:
+//    Window(QWidget *parent = 0);
+//
+//private:
+//
+//};
 
 
 class MyWidget : public QWidget
 {
-    public:
-        MyWidget(QWidget *parent = 0);
+public:
+    MyWidget(QWidget *parent = 0);
 };
 
 
